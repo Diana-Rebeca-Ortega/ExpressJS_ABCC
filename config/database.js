b58cbe624f4679799862d99b82aa70f84b14e4ea
Diana Rebeca Ortega <diana.os@tecjerez>
2026-03-20 13:19:28 -0600

    Se borro la base de datos inicial

--- config/database.js
+++ config/database.js
@@ -1,20 +1,3 @@
-/*const mysql = require('mysql2');
-const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1819diana',
-    database: 'BD_Express_2026'
-});
-
-conexion.connect(function(err){
-    if(err)
-        throw err;
-    console.log('Conexion a BD exitosa!!!');
-});
-
-module.exports = conexion;
-*/
-
 const mysql = require('mysql2');
 
 const conexion = mysql.createConnection({
