9fe0e01861bb332aa8523d124fe5fb47e3d66f5a
Diana Rebeca Ortega <diana.os@tecjerez>
2026-03-20 15:17:57 -0600

    Cambios

--- models/alumno.js
+++ models/alumno.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const conexion =require('../config/database');
+const conexion = require('../config/database');
+
 let Alumno = function(alumno){
-    // Estos nombres deben ser IGUALES a tu tabla en MySQL
     this.NumControl = alumno.NumControl; 
     this.Nombre     = alumno.Nombre;
     this.PrimerAp   = alumno.PrimerAp;
@@ -12,70 +12,69 @@ let Alumno = function(alumno){
     this.Carrera    = alumno.Carrera;
 };
 
-//===================LOGICA PARA LA BD DE DATOS RELACIONAL========
+// --- CONSULTA DE TODOS (El que fallaba en el forEach) ---
+Alumno.findAll = function(result){
+    // Al usar el pool, es mejor manejar el error directamente aquí
+    conexion.query("SELECT * FROM alumnos", function(err, res){
+        if(err){
+            console.error("Error en findAll:", err);
+            return result(err, null);
+        }
+        // Aseguramos que siempre devolvemos un arreglo
+        result(null, res || []);
+    });
+};
+
+// --- BUSQUEDA POR ID ---
+Alumno.findById = function(nc, result){
+    conexion.query("SELECT * FROM alumnos WHERE NumControl = ?", [nc], function(err, res){
+        if(err){
+            console.error("Error en findById:", err);
+            return result(err, null);
+        }
+        // res[0] porque SELECT devuelve un arreglo y solo queremos un alumno
+        result(null, res[0] || {});
+    });
+};
+
+// --- ALTA ---
 Alumno.create = function(alumno, result){
     conexion.query("INSERT INTO alumnos SET ?", alumno, function(err, res){
-            if(err){
-                console.log("Error: ", err);
-                result(err, null);
-            }else{
-                console.log("Insrcion EXITOSA");
-                result(null, res.insertId);
-            }
+        if(err){
+            console.error("Error en create:", err);
+            result(err, null);
+        } else {
+            result(null, res.insertId);
+        }
     });
-}
+};
 
-//BAJAS
+// --- BAJA ---
 Alumno.delete = function(nc, result){
-    conexion.query("DELETE FROM alumnos WHERE NumControl= ?", [nc], function(err, res){
-     if(err){
-                console.log("Error: ", err);
-                result(null,err);
-            }else{
-                console.log("Eliminacion EXITOSA");
-                result(null, res.insertID);
-            }
+    conexion.query("DELETE FROM alumnos WHERE NumControl = ?", [nc], function(err, res){
+        if(err){
+            console.error("Error en delete:", err);
+            result(err, null);
+        } else {
+            result(null, res);
+        }
     });
+};
 
-}
-//CAMBIOS 
+// --- CAMBIOS ---
 Alumno.update = function(id, alumno, result){
-    conexion.query("UPDATE alumnos SET Nombre = ?, PrimerAp =?, SegundoAp=?, FechaNac=?, Semestre=?, Carrera =? WHERE NumControl = ?",
-        [alumno.Nombre, alumno.PrimerAp, alumno.SegundoAp, alumno.FechaNac, alumno.Semestre, alumno.Carrera, id], function(err,res){
+    conexion.query(
+        "UPDATE alumnos SET Nombre = ?, PrimerAp = ?, SegundoAp = ?, FechaNac = ?, Semestre = ?, Carrera = ? WHERE NumControl = ?",
+        [alumno.Nombre, alumno.PrimerAp, alumno.SegundoAp, alumno.FechaNac, alumno.Semestre, alumno.Carrera, id], 
+        function(err, res){
             if(err){
-                console.log("Error: ", err);
+                console.error("Error en update:", err);
                 result(err, null);
-            }else{
-                console.log("Modificacion EXITOSA");
+            } else {
                 result(null, res);
             }
         }
-    )
-}
-//CONSULTAS
+    );
+};
 
-Alumno.findById = function(nc, result){
-    conexion.query("SELECT * FROM alumnos WHERE NumControl =?", [nc], function(err, res){
-          if(err){
-                console.log("Error: ", err);
-                result(err, null);
-            }else{
-                console.log("Modificacion EXITOSA");
-                result(null, res);
-            }
-    })
-}
-
-Alumno.findAll = function(result){
-    conexion.query("SELECT * FROM alumnos", function(err, res){
-          if(err){
-                console.log("Error: ", err);
-                result(err, null);
-            }else{
-                console.log("Datos encontrados:", res);
-                result(null, res);
-            }
-    })
-}
-
-module.exports  = Alumno;
+module.exports = Alumno;
