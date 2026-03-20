40e10547261dba1d900fa5b7fceeb85058477ef2
Diana Rebeca Ortega <diana.os@tecjerez>
2026-03-20 13:44:38 -0600

    Se modifico el database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 
-const conexion = mysql.createConnection({
+const conexion = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
