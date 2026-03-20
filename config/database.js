201d91c57bde6dff37b7cccb7926403218c8bf1e
Diana Rebeca Ortega <diana.os@tecjerez>
2026-03-20 13:01:15 -0600

    Se configuro la base de datos para conectarla a render

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -10,6 +10,27 @@ conexion.connect(function(err){
     if(err)
         throw err;
     console.log('Conexion a BD exitosa!!!');
+});
+
+module.exports = conexion;
+*/
+
+const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 3306 // Puerto estándar de Clever Cloud
+});
+
+conexion.connect(function(err){
+    if(err){
+        console.error('Error de conexión a la BD:', err);
+        return;
+    }
+    console.log('¡Conexión a BD en la nube exitosa!');
 });
 
 module.exports = conexion;
